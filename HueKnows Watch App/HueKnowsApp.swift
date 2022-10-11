e46b21ae7e75dce56e11d257bfa415cd92ecc6bb
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-11 19:09:22 +0200

    Initial Commit

---
+++ HueKnows Watch App/HueKnowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueKnowsApp.swift
+//  HueKnows Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-11.
+//
+
+import SwiftUI
+
+@main
+struct HueKnows_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
